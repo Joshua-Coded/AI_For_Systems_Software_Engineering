65ea59f83d2be3f0cba4571332b14cf467aba14f
joshbam-joshwilly <j.alana@alustudent.com>
2025-03-21 19:48:09 +0200

    exercise

--- Day2/day2.py
+++ Day2/day2.py
@@ -35,13 +35,60 @@
        
 # check if a given number is a prime number (if it is not divisible by 2)
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
 
-if num > 1:
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            print(f"{num} is not a prime")
-            break
-        else:
-            print(f"{num} is a prime")
+# if num > 1:
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             print(f"{num} is not a prime")
+#             break
+#         else:
+#             print(f"{num} is a prime")
+
+
+# def add(a, b):
+#     return a + b
+
+# def sub(a, b):
+#     return a - b
+
+# def mul(a, b):
+#     return a * b
+
+# def div(a, b):
+#     if b != 0:
+#      return a // b
+    
+#     else:
+#         return "Division by zero is not allowed" 
+    
+    
+# while True:
+#     print("\nMenu:")
+#     print("1. Addition")
+#     print("2. Subtraction")
+#     print("3. Multiplication")
+#     print("4 Division")
+#     print("5. Exit")
+    
+#     choice = input("Enter choice from the menu")
+    
+#     if choice == "5":
+#         print("Exitin program")
+#         break
+    
+#     num1 = float(input("Enter the first number"))
+#     num2 = float(input("Enter the second number"))
+    
+#     if choice == "1":
+#         print("Result:", add(num1. num2))
+#     elif choice == "2":
+#         print("Result:", sub(num1, num2))
+#     elif choice == "3":
+#         print("Resutl: ", mul(num1, num2))
+#     elif choice == "4":
+#         print("Result: ", div(num1, num2))
+#     else:
+#         print("Invalid choice:. please try again")
+    

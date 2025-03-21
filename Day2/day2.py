703514c82d65e8682790c47af20f298f460e26bc
joshbam-joshwilly <j.alana@alustudent.com>
2025-03-21 19:16:15 +0200

    looping

---
+++ Day2/day2.py
@@ -0,0 +1,13 @@
+# Looping  checks
+
+# Loop through a list of items
+
+fruits = ["Apple", "Banana", "Cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+    
+
+
+for i in range(5):
+    print(i)
